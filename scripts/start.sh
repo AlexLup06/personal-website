94080dfd66c447db791974f8aa7b4c0cd349e519
AlexLup06 <alexander@lupatsiy.de>
2025-01-09 22:27:30 +0000

    add PWD to docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,5 @@
 make
 
 # Run docker compose
-docker-compose -f docker-compose.prod.yml build
-docker-compose -f docker-compose.prod.yml up -d
+PWD=${PWD} docker-compose -f docker-compose.prod.yml build
+PWD=${PWD} docker-compose -f docker-compose.prod.yml up -d
