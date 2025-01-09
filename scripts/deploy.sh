446a7bc3b1c3e99305c1d2550b7c5bcb9e9b5175
Alexander Lupatsiy <alexander@lupatsiy.de>
2025-01-09 23:40:46 +0100

    add pwd to command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,18 +11,18 @@ echo "$(date): Releasing new server version. $BUILD_VERSION"
 
 echo "$(date): Running build..."
 make
-PWD=${PWD} sudo docker compose -f docker-compose.prod.yml rm -f
-PWD=${PWD} sudo docker compose -f docker-compose.prod.yml build
+env PWD=${PWD} sudo docker compose -f docker-compose.prod.yml rm -f
+env PWD=${PWD} sudo docker compose -f docker-compose.prod.yml build
 
 OLD_CONTAINER=$(sudo docker ps -aqf "name=server")
 echo "$(date): Scaling server up..."
-BUILD_VERSION=$BUILD_VERSION PWD=${PWD} sudo docker compose -f docker-compose.prod.yml up -d --no-deps --scale server=2 --no-recreate server
+env PWD=${PWD} sudo docker compose -f docker-compose.prod.yml up -d --no-deps --scale server=2 --no-recreate server
 
 sleep 30
 
 echo "$(date): Scaling old server down..."
-PWD=${PWD} sudo docker container rm -f $OLD_CONTAINER
-PWD=${PWD} sudo docker compose -f docker-compose.prod.yml up -d --no-deps --scale server=1 --no-recreate server --remove-orphans
+env PWD=${PWD} sudo docker container rm -f $OLD_CONTAINER
+env PWD=${PWD} sudo docker compose -f docker-compose.prod.yml up -d --no-deps --scale server=1 --no-recreate server --remove-orphans
 
 echo "$(date): Reloading caddy..."
 CADDY_CONTAINER=$(sudo docker ps -aqf "name=caddy")
