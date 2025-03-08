dfa836a13b3120e62987309c09ea88d4b97cce57
Alexander Lupatsiy <alexander@lupatsiy.de>
2025-03-08 20:53:54 +0100

    fix logos in safari

--- frontend/src/views/homepage/TechStack_templ.go
+++ frontend/src/views/homepage/TechStack_templ.go
@@ -253,7 +253,7 @@ func BestTech(name string, icon templ.Component, side string) templ.Component {
 			templ_7745c5c3_Var8 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<div class=\"center px-3 py-1.5 md:px-4 md:py-2 bg-third-300 text-white items-center rounded-xl text-md md:text-lg flex gap-2\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<div class=\"center px-3 py-1.5 md:px-4 md:py-2 bg-third-300 text-white items-center rounded-xl text-md md:text-lg flex gap-2 \">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
