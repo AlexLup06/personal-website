d6fee3e41d4a01d8e3f4236c86d8396b0c93b0ce
Alexander Lupatsiy <alexander@lupatsiy.de>
2025-03-10 09:39:15 +0100

    finally solved stack scroll bug

--- frontend/src/ts/techStack.ts
+++ frontend/src/ts/techStack.ts
@@ -3,18 +3,19 @@ window.htmx.onLoad(() => {
     const rightButton = document.getElementById("right-button") as HTMLButtonElement
     const stackContainer = document.getElementById("stack-container") as HTMLDivElement
     const scrollContainer = document.getElementById("scroll-stack-container") as HTMLDivElement
+    const stackElement = stackContainer.children[0] as HTMLDivElement
 
     if (leftButton == null || rightButton == null || stackContainer == null) {
         return
     }
 
-    const scrollAmount = 240
+    const scrollAmount = stackElement.getBoundingClientRect().width
 
     leftButton.addEventListener("click", () => handleLeftClick())
     rightButton.addEventListener("click", () => handleRightClick())
 
     function handleLeftClick() {
-        if (scrollContainer.scrollLeft - 240 < 0) {
+        if (scrollContainer.scrollLeft - scrollAmount < 0) {
             scrollContainer.scrollTo({
                 top: 0,
                 left: 0,
@@ -27,14 +28,13 @@ window.htmx.onLoad(() => {
 
     function handleRightClick() {
         const stackContainerWidth = stackContainer.getBoundingClientRect().width
-        console.log("conteiner width: ", stackContainerWidth)
-        console.log("scroll left in stackContainer: ", scrollContainer.scrollLeft)
 
         if (scrollContainer.scrollLeft == 0) {
             scrollContainer.scrollBy({ left: 100, behavior: "smooth" })
+            return
         }
 
-        if (scrollContainer.scrollLeft + 240 > stackContainerWidth) {
+        if (scrollContainer.scrollLeft + scrollAmount > stackContainerWidth) {
             scrollContainer.scrollTo({
                 top: 0,
                 left: stackContainerWidth,
